9523cd7f51ae4388c3d9e31e32a3895171ef48bf
Elitekobold <155539450+Elitekobold@users.noreply.github.com>
2026-03-09 08:58:05 +0100

    Use only Microsoft.Data.Sqlite package for DB connectivity

--- HardwareChecker/App.xaml.cs
+++ HardwareChecker/App.xaml.cs
@@ -1,14 +1,8 @@
 using System.Windows;
-using SQLitePCL;
 
 namespace HardwareChecker
 {
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            Batteries_V2.Init();
-            base.OnStartup(e);
-        }
     }
 }
